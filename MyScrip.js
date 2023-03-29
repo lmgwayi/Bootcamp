9b2afc2b7cfd84068a81701e72e70a179f7b2037
luxolo mgwayi <lmgwayi1225@gmai.com>
2023-03-29 16:08:55 +0200

    new updates

---
+++ MyScrip.js
@@ -0,0 +1,37 @@
+//var a= 1;
+//var a= 10;
+// let a= 1;
+// let a= 10;
+// console.log (a);
+// let a= 1;
+// let b= 2;
+// let c= a+b;
+// console.log(c);
+//let d= 10
+//let c= 10
+//let a= 70;
+//let b= 20;
+//if (a==c || c==d) {
+//console.log (true);
+//}
+//else {
+// console.log("false");
+ //}
+
+//let a= 10
+ //if (a>10){
+// console.log ("Greater than a decade");
+// } else if (a==-10) {
+//  console.log ("if is a decade");
+//} else if (a<10 && a<=0){
+//    console.log ("if is lesser than a decade")
+// } else {
+//    console.log ("we cannot have negative year!")
+// }
+
+let a= 15
+let b= 50
+let c= 4
+if (a<b && c<b );{
+console.log(b);
+}
