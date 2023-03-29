9101b921e2e065ad93f0ffaadf4b7155e5439fae
luxolo mgwayi <lmgwayi1225@gmai.com>
2023-03-29 15:59:21 +0200

    initial commit

--- Activityfolder/java.js
+++ Activityfolder/java.js
@@ -1,4 +1,14 @@
-function greet() {
-    var name = document.getElementById("search").value;
-    alert("Hello",WebSearcher);
-}
+
+<html>
+<body>
+
+<h2>My First JavaScript</h2>
+
+<button type="button"
+onclick="document.getElementById('demo').innerHTML = Date()">
+Click me to display Date and Time.</button>
+
+<p id="demo"></p>
+
+</body>
+</html> 
