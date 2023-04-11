8f97f2019bd23859cc5527323312e3365debdb11
luxolo mgwayi <lmgwayi1225@gmai.com>
2023-04-11 17:25:26 +0200

    new updates

--- Animation/anime.js
+++ Animation/anime.js
@@ -28,20 +28,31 @@
 // prev()
 // prev()
 
-  let slideIndex = 1;
-    showDivs(slideIndex);
-    
-    function plusDivs(n) {
-    showDivs(slideIndex += n);
-    }
-    
-   function showDivs(n) {
-     let index;
-      let images = document.getElementsByClassName("mySlides");
-      if (n > images.length) {slideIndex = 1}
-      if (n < 1) {slideIndex = images.length}
-      for (i = 0; i < images.length; i++) {
-        images[i].style.display = "none";  
-      }
-      images[slideIndex-1].style.display = "block";  
-    }
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
