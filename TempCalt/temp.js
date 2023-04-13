f820d1d68b51161609739eb298f4db06fd1cd659
luxolo mgwayi <lmgwayi1225@gmai.com>
2023-04-13 16:17:26 +0200

    new updates

--- TempCalt/temp.js
+++ TempCalt/temp.js
@@ -1,22 +1,56 @@
 //   HOW TO CREATE A FUNCTION?? 
 
-//function cToF (c){
-// return F = c*/5+35
-//}
-//console.log(cToF(15))
+// F = c*9/5+35
+
+// function cToF (){
+
+// alert (F)
+
+// }
+// cToF()
+// console.log(cToF(15))
 
 function convert(){
  let f = document.getElementById("entercelsius").value
- let c = (f-30) * 5/9;
+ let c = (f-32) * 5/9;
  
- if (c<16){
-    alert("it is cold, wear jacket");
+ if(c<16){
+   alert(c+"it is cold, wear jacket");
+ }else if(c >=16 && c<21){
+   alert(c+"it is warm, wear t-shirt + jeans");
+ }else {
+   alert(c+"it is hot, wear shorts");
  }
- else if(c<16 && c<21){
-    alert("it is warm, wear t-shirt + jeans");
- }
- else {
-    alert("it is hot, wear shorts");
- }
- convert(50)
+ 
 }
+convert()
+
+// function calculateTemp() {
+//    function celToFah(celsius) {
+//       let fahrenheit = Math.round((celsius * (9/5)) + 32);
+//       return fahrenheit;
+//    }
+
+//    function fahToCel(fahrenheit) {
+//       let celsius = Math.round((fahrenheit - 32) * (5/9));
+//       return celsius;
+//    }
+
+//    // Checks if Calsius or Fahrenheit is selected
+//    let tempSelected = document.getElementById("temp_diff");
+//    let valueTemp = tempSelected.options[tempSelected.selectedIndex].value;
+
+//    // Checks the value inside the Temperature input
+//    let numberTemp = document.getElementById("temp").value;
+
+//    let result;
+   
+//    if (valueTemp == 1) {
+//       result = celToFah(numberTemp);
+//       document.getElementById("resultContainer").innerHTML = "= " + result + "°Fahrenheit";
+//    } else {
+//       result = fahToCel(numberTemp);
+//       document.getElementById("resultContainer").innerHTML = "= " + result + "°Celsius";
+//    }
+   
+// }
